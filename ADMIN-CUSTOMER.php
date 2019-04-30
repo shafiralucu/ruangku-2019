654dc7a8e3285ecd36359397ff85e0ff55e8e7e3
MUHAMMAD ALIF KHADAFFA <i16074@ftis>
2019-04-30 16:00:50 +0700

    .

--- ADMIN-CUSTOMER.php
+++ ADMIN-CUSTOMER.php
@@ -282,6 +282,7 @@
                 echo "<td>".$row['no_hp']."</td>";
                 echo "<td>".$row['alamat']."</td>";
                 echo "<td>".$row['email']."</td>";
+                echo "<td>".$row['tanggalTransaksi']."</td>";
                 
 				echo "</tr>";
 			}
@@ -315,7 +316,7 @@
                 <label><b>Email</b></label>
                 <input class="w3-input w3-border" type="text" placeholder="Email Customer" name="email" required> <br>
                 <label><b>Nama Ruangan</b></label>
-                <input class="w3-input w3-border" type="text" placeholder="Nama Ruangan" name="email" required> <br>
+                <input class="w3-input w3-border" type="text" placeholder="Nama Ruangan" name="namaR" required> <br>
                 <label><b>Tanggal transaksi</b></label>
                 <input class="w3-input w3-border" type="date" name="tanggaltransaksi" required> <br>
                 <label><b>Total Transaksi</b></label> 
--- Model/insertCustomer.php
+++ Model/insertCustomer.php
@@ -6,10 +6,6 @@
             $no_hp = $_POST['nohp'];
             $tanggaltransaksi = $_POST['tanggaltransaksi'];
             $alamat = $_POST['alamat'];
-                $namaRuangan = $db->escapeString($namaRuangan);
-                $kapasitas = $db->escapeString($kapasitas);
-                $fasilitas = $db->escapeString($fasilitas);
-                $harga = $db->escapeString($harga);
 				$query = "INSERT INTO pelanggan (nama, alamat, email, no_hp , tanggal_transaksi) VALUES ('$nama' , '$alamat' , '$email','$no_hp' , '$tanggaltransaksi')";
                 $db->executeNonSelectedQuery($query);
                 header('Location: ../ADMIN-CUSTOMER.php');
