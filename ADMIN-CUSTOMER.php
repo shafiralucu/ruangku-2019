63b39dc35d6263468647207b6a7ca73e81b1fa9e
GIOVANNI ANGGASTA <i16049@ftis>
2019-04-30 15:57:39 +0700

    tabel baru

--- ADMIN-CUSTOMER.php
+++ ADMIN-CUSTOMER.php
@@ -264,8 +264,8 @@
                 <th>Nama</th>
                 <th>No. Handphone</th>
                 <th>Alamat</th>
-                <th>Ruangan</th>
                 <th>E-mail</th>
+                <th>Tanggal Transaksi</th>
               </tr>
             </thead>
             <tr>
@@ -375,8 +375,6 @@
                 <input class="w3-input w3-border" type="text" placeholder="Alamat Customer" name="alamat" required> <br>
                 <label><b>Email</b></label>
                 <input class="w3-input w3-border" type="text" placeholder="Email Customer" name="email" required> <br>
-                <label><b>Nama Ruangan</b></label>
-                <input class="w3-input w3-border" type="text" placeholder="Nama Ruangan" name="email" required> <br>
                 <label><b>Tanggal transaksi</b></label>
                 <input class="w3-input w3-border" type="date" name="tanggaltransaksi" required> <br>
                 <label><b>Total Transaksi</b></label> 
--- CONTACT-COMPLETE.php
+++ CONTACT-COMPLETE.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Contact</title>
     <style>
         @font-face {
@@ -26,7 +28,7 @@
 
 <body>
     
-    <h1 class="w3-center">Thankyou for your </h1>
+<div class="w3-display-middle">Thankyou for your kindness !</div>
 
 </body>
 
