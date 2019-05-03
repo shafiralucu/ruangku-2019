8d8a35a7c2e25daa1f76d51f7f79c1520ce8da87
alifkhadaffa <alifkhadaffaaa@gmail.com>
2019-05-03 19:09:39 +0700

    sudah masuk semua ke tabel2 basis data foreign key

--- Model/insertTransaksiOP.php
+++ Model/insertTransaksiOP.php
@@ -5,9 +5,10 @@
             //untuk update ruangan sehingga status book berubah
             $namaRuang = $_POST['ruangan'];
             $queryUpdateRuangan = "UPDATE ruang SET status_booking = '1' WHERE namaRuang = '$namaRuang'";
+            $db->executeNonSelectedQuery($queryUpdateRuangan);
             
 
-            //untuk pelanggan
+            //untuk mendapat value pelanggan
             $nama = $_POST['nama'];
             $email = $_POST['email'];
             $no_hp = $_POST['nohp'];
@@ -21,12 +22,15 @@
             $email = $db->escapeString($email);
             $no_hp = $db->escapeString($no_hp);
             $alamat = $db->escapeString($alamat);
+            $waktuMulai = $db->escapeString($waktuMulai);
+            $waktuAkhir = $db->escapeString($waktuAkhir);
 
-            //untuk update alat
+            //untuk update alat sehingga status book alat berubah
             $choices = $_POST['check_list'];
             for ($i=0; $i<sizeof ($choices);$i++) {  
                 $query3="UPDATE alat SET status_booking = '1' WHERE namaAlat = '".$choices[$i]. "'"; 
             }
+            $db->executeNonSelectedQuery($query3);
 
             //tanggal
             date_default_timezone_set("Asia/Bangkok");
@@ -41,54 +45,38 @@
                 $res =$row[0];
             }
 
-            //menghitung total transaksi
-            $totalTransaksi = $res * ($waktuAkhir - $waktuMulai);
-
-            //query insert
-            $query = "INSERT INTO pelanggan (nama, alamat, email, no_hp) VALUES ('$nama' , '$alamat' , '$email','$no_hp')";
-            $query2 = "INSERT INTO transaksi (tanggal_transaksi , waktu_awal , waktu_akhir , total_transaksi) VALUES ('$my_date' , '$waktuMulai' , '$waktuAkhir' , '$totalTransaksi')";
-<<<<<<< HEAD
-            $db->executeNonSelectedQuery($query);
-            $db->executeNonSelectedQuery($query2);
-            //untuk get id fk dan memasukkan ke tabel fk(tabel relasi)
-            $count = "SELECT COUNT(idTransaksi) FROM transaksi";
-            $temp = $db->executeNonSelectedQuery($count);
-             $resultqget = "";
-             while ($row=mysqli_fetch_row($temp))
-             {
-             $resultqget = $row[0];
-             }  
-             $resultqget = $resultqget+1;
-
-
-            $queryGet2 = "SELECT idPelanggan FROM pelanggan WHERE nama = '$nama'";
-            $temp2 = $db->executeNonSelectedQuery($queryGet2);
-            $resultqget2 = "";
-            while ($row=mysqli_fetch_row($temp2))
-=======
-
-
-            //berhasil//
-
-
-
-            //untuk get id fk dan memasukkan ke tabel fk(tabel relasi)
-            $queryGet = "SELECT idTransaksi FROM transaksi WHERE tanggal_transaksi = '$my_date'";
-            $resultIdTransaksi = $db->executeNonSelectedQuery($queryGet);
-            $res1="";
-            while ($row=mysqli_fetch_row($resultIdTransaksi))
->>>>>>> c2192b12a2e46c22ac5fa70dc5f4d61fcd5fcd76
+            //query get harga alat
+            for ($i=0; $i<sizeof ($choices);$i++) {  
+                $qGetHargaAlat = "SELECT tarif FROM alat WHERE namaAlat = '".$choices[$i]. "'"; 
+            }
+            
+            $resultHargaAlat = $db->executeNonSelectedQuery($qGetHargaAlat);
+            $resHargaAlat="";
+            while ($row=mysqli_fetch_row($resultHargaAlat))
             {
-                $res1 = $row[0];
+                $resHargaAlat =$row[0];
             }
 
-<<<<<<< HEAD
-            // echo $resultqget2;
-            // echo $resultqget2;
-            // echo $resultqget;
-            
-            $q = "INSERT INTO melakukan  VALUES ($resultqget2 , $resultqget)";
-=======
+            //menghitung total transaksi
+            $totalTransaksi = ($res * ($waktuAkhir - $waktuMulai)) + $resHargaAlat;
+
+            //query insert ke tabel pelanggan dan transaksi
+            $query = "INSERT INTO pelanggan (nama, alamat, email, no_hp) VALUES ('$nama' , '$alamat' , '$email','$no_hp')";
+            $query2 = "INSERT INTO transaksi (tanggal_transaksi , waktu_awal , waktu_akhir , total_transaksi) VALUES ('$my_date' , '$waktuMulai' , '$waktuAkhir' , '$totalTransaksi')";
+            $db->executeNonSelectedQuery($query);
+            $db->executeNonSelectedQuery($query2);
+
+            //berhasil sampai baris sini, code works fine//
+
+            //untuk get id fk dan memasukkan ke tabel fk(tabel relasi)
+            $queryGet = "SELECT idTransaksi FROM transaksi";
+            $resultIdTransaksi = $db->executeNonSelectedQuery($queryGet);
+            $res1="";
+              while ($row=mysqli_fetch_row($resultIdTransaksi))
+              {
+                  $res1 = $row[0];
+              }
+
             $queryGet2 = "SELECT idPelanggan FROM pelanggan WHERE nama = '$nama'";
             $resultIdPelanggan = $db->executeNonSelectedQuery($queryGet2);
             $res2="";
@@ -96,28 +84,34 @@
             {
                 $res2 = $row[0];
             }
->>>>>>> c2192b12a2e46c22ac5fa70dc5f4d61fcd5fcd76
+
+            $q = "INSERT INTO melakukan (idPelanggan , idTransaksi) VALUES ('$res2' , '$res1')";
+            $db->executeNonSelectedQuery($q);
+
+            //Query ke tabel Sewa Alat
+            for ($i=0; $i<sizeof ($choices);$i++) {  
+                $queryGetAlat = "SELECT idAlat FROM alat WHERE namaAlat = '".$choices[$i]. "'";
+            }
+            $resultIdAlat = $db->executeNonSelectedQuery($queryGetAlat);
+            $res3="";
+            while ($row=mysqli_fetch_row($resultIdAlat))
+            {
+                $res3 = $row[0];
+            }
+            $querySewaAlat = "INSERT INTO sewa_alat (idAlat , idTransaksi) VALUES('$res3' , '$res1')";
+            $db->executeNonSelectedQuery($querySewaAlat);
+
+            //Query ke tabel Sewa Ruang
+            $queryGetRuang = "SELECT idRuang FROM ruang WHERE namaRuang = '$namaRuang'";
+            $resultIdRuang = $db->executeNonSelectedQuery($queryGetRuang);
+            $res4="";
+            while ($row=mysqli_fetch_row($resultIdRuang))
+            {
+                $res4 = $row[0];
+            }
+            $querySewaRuang = "INSERT INTO sewa_ruang (idRuang , idTransaksi) VALUES('$res4' , '$res1')";
+            $db->executeNonSelectedQuery($querySewaRuang);
             
-            // $db->executeSelectQuery($queryGet2);
-            // $db->executeSelectQuery($queryGet);
-            $q = "INSERT INTO melakukan(idPelanggan , idTransaksi) VALUES($res1 , $res2)";
-            
-<<<<<<< HEAD
-
-            //query insert
-
-   
-=======
-            //$rows = mysql_fetch_assoc($queryGet2);
-            // //query insert
-            // $db->executeSelectQuery($queryGet2);
-            $db->executeNonSelectedQuery($query);
-            $db->executeNonSelectedQuery($query2);
->>>>>>> c2192b12a2e46c22ac5fa70dc5f4d61fcd5fcd76
-            $db->executeNonSelectedQuery($query3);
-
-            $db->executeNonSelectedQuery($queryUpdateRuangan);
-            // $db->executeNonSelectedQuery($q);
-            // header('Location: ../View/OPERATOR-COMPLETE.php');
+            header('Location: ../View/OPERATOR-COMPLETE.php');
         }
 ?>
